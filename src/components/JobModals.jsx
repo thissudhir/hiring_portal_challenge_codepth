7953456c0b987226342ce39aeee54cb58ba97249
Abhishek Yadav <abhishekpsitfor4years@gmail.com>
2023-12-23 23:41:31 +0530

    created modals

--- src/components/JobModals.jsx
+++ src/components/JobModals.jsx
@@ -1,10 +1,23 @@
-import { Dialog, DialogTitle } from "@mui/material";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  FilledInput,
+  Grid,
+} from "@mui/material";
 import React from "react";
 
 const JobModals = () => {
   return (
     <Dialog>
       <DialogTitle>Post Job</DialogTitle>
+      <DialogContent>
+        <Grid container>
+          <Grid item xs={6}>
+            <FilledInput placeholder="Job title" disableUnderline />
+          </Grid>
+        </Grid>
+      </DialogContent>
     </Dialog>
   );
 };
